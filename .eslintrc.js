2ecd180295431b9e0baff3a397cbd886e25d9310
ledhed2222 <ledhed2222@users.noreply.github.com>
2022-02-17 01:13:56 -0500

    upgrade deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,8 +21,5 @@ module.exports = {
   rules: {
     'jsdoc/require-jsdoc': 'off',
     '@typescript-eslint/naming-convention': 'off',
-
-    // TODO can't figure out how to get this to work with tsx
-    '@typescript-eslint/no-unused-vars-experimental': 'off',
   },
 }
