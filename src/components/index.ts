4a317dc3c3197dd6b420ff1c3b917785848fa2f0
ledhed2222 <ledhed2222@users.noreply.github.com>
2025-12-04 00:58:03 -0500

    testing

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as ExternalLink } from './ExternalLink'
 export { default as Header } from './Header'
 export { default as Loading } from './Loading'
-export {
-  default as Paragraph } from './Paragraph'
+export { default as Paragraph } from './Paragraph'
