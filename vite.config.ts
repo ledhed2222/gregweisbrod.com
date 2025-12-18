543d4716e6ee021552736a5aaea521ca04d978b0
ledhed2222 <ledhed2222@users.noreply.github.com>
2025-12-18 04:39:15 -0500

    stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,22 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'well-known-redirect',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/.well-known' || req.url === '/.well-known/') {
+            res.writeHead(302, { Location: '/' })
+            res.end()
+            return
+          }
+          next()
+        })
+      },
+    },
+  ],
   server: {
     port: 3000,
     open: true,
