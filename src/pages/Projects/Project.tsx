6c741023e452f0c07b5abcb7f6fcd86608845fed
ledhed2222 <ledhed2222@users.noreply.github.com>
2026-07-17 23:35:30 -0400

    lazy-load project images and add descriptive alt text

--- src/pages/Projects/Project.tsx
+++ src/pages/Projects/Project.tsx
@@ -5,15 +5,27 @@ import { ExternalLink, Header } from '../../components'
 interface Props {
   title: string
   imageSrc: string
+  imageAlt: string
   link: string
   children: ReactNode
 }
 
-export default function Project({ link, title, imageSrc, children }: Props) {
+export default function Project({
+  link,
+  title,
+  imageSrc,
+  imageAlt,
+  children,
+}: Props) {
   return (
     <div className="Project">
       <ExternalLink href={link}>
-        <img src={imageSrc} className="ProjectImg" alt="" />
+        <img
+          src={imageSrc}
+          className="ProjectImg"
+          alt={imageAlt}
+          loading="lazy"
+        />
       </ExternalLink>
       <div className="ProjectCopy">
         <ExternalLink href={link}>
